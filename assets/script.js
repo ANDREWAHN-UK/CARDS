f19536bf3f57c75c57d00dd331ab22096a069e88
ANDREWAHN-UK <ANDREW.AHN@HOTMAIL.CO.UK>
2021-07-30 17:10:48 +0000

    script.js - added functionality to remove active class from panels, then add active class to a panel that is clicked on

---
+++ assets/script.js
@@ -0,0 +1,20 @@
+// this takes all the panels and places them in a variable
+const panels = document.querySelectorAll(".panel");
+
+// this is basically a for loop that goes through each panel, removes the active class and adds it to the panel clicked on
+panels.forEach(
+    (panel) => {
+        panel.addEventListener('click', () => {
+            removeActiveClasses()
+            panel.classList.add("active");
+        })
+    }
+)
+
+
+// this function removes the active class from a panel
+function removeActiveClasses() {
+    panels.forEach((panel) => {
+        panel.classList.remove("active");
+    })
+}
